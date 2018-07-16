1c951a2239b176f4de4424853656926024d0796d
Rob Gifford <robjgiff@gmail.com>
2018-07-16 12:16:15 +0100

    Initial commit for Flaviviridae

---
+++ glue/loadExogenousData.js
@@ -0,0 +1,13 @@
+// list the sequences in source ncbi-refseqs
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs'"]);
+// extract from the result a list of sequence IDs.
+var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+// for each sequence ID
+_.each(seqIds, function(seqId) {
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "dfe_exogenous_data", seqId]);
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/ncbi-refseqs/"+seqId, function() {
+        glue.command(["set", "link-target", "dfe_exogenous_data", "custom-table-row/dfe_exogenous_data/"+seqId]);
+    });
+});
