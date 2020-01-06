cd90102da1bba069ec7333ddab6f27f8ca9f8dbb
Rob Gifford <robjgiff@gmail.com>
2020-01-06 15:28:43 +0000

    Updated project build

--- glue/eve/loadLocusData1.js
+++ glue/eve/loadLocusData1.js
@@ -1,5 +1,5 @@
 // list the flavivirus EVE sequences
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-curated'"]);
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-eve'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
 
@@ -8,7 +8,7 @@ _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "endogenous_data", seqId]);
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/fasta-curated/"+seqId, function() {
+    glue.inMode("sequence/fasta-eve/"+seqId, function() {
         glue.command(["set", "link-target", "endogenous_data", "custom-table-row/endogenous_data/"+seqId]);
     });
 });
