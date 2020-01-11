fcfb032ce0143e7274752a7e9ff14faee72ec360
Rob Gifford <robjgiff@gmail.com>
2020-01-11 14:19:29 +0000

    Merge

---
+++ glue/loadLocusData2.js
@@ -0,0 +1,22 @@
+// Load EVE data from tab file 
+var loadResult;
+glue.inMode("module/flaviviridaeTabularUtility", function() {
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/eve/efv-side-data.tsv"]));
+	//glue.log("INFO", "load result was:", loadResult);
+});
+
+_.each(loadResult, function(eveObj) {
+
+	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
+	
+		glue.command(["set", "field", "scaffold", eveObj.scaffold]);
+		glue.command(["set", "field", "start_position", eveObj.start]);
+		glue.command(["set", "field", "end_position", eveObj.end]);
+		glue.command(["set", "field", "orientation", eveObj.orientation]);
+		glue.command(["set", "field", "length", eveObj.length]);
+		glue.command(["set", "field", "integration", eveObj.integration]);
+		glue.command(["set", "field", "fragment", eveObj.fragment]);
+		glue.command(["set", "field", "duplicate", eveObj.duplicate]);
+
+	});
+});
