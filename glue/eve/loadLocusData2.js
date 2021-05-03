04cf7e5e31dbf193c622c9e0bd8bc5e470fdb6af
Rob Gifford <robjgiff@gmail.com>
2021-05-03 18:47:11 +0100

    Refactor

--- glue/eve/loadLocusData2.js
+++ glue/eve/loadLocusData2.js
@@ -10,15 +10,28 @@ _.each(loadResult, function(eveObj) {
 	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
 
 	    glue.log("INFO", "eveObj result was:", eveObj);
-	
+
+
+		glue.command(["set", "field", "locus_id", eveObj.locus_numeric_id]);	
+		glue.command(["set", "field", "duplicate_id", eveObj.duplicate_id]);	
 		glue.command(["set", "field", "scaffold", eveObj.scaffold]);
 		glue.command(["set", "field", "start", eveObj.extract_start]);
 		glue.command(["set", "field", "end", eveObj.extract_end]);
 		glue.command(["set", "field", "orientation", eveObj.orientation]);
-		//glue.command(["set", "field", "integration", eveObj.integration]);
-	    //glue.command(["set", "field", "fragment", eveObj.fragment]);
-		//glue.command(["set", "field", "duplicate", eveObj.duplicate]);
 
 	});
+	
+
+	glue.inMode("sequence/fasta-digs-eve/"+eveObj.sequenceID, function() {
+	
+		glue.command(["set", "field", "name", eveObj.sequenceID]);
+		glue.command(["set", "field", "full_name", eveObj.sequenceID]);
+		glue.command(["set", "field", "species", eveObj.organism]);
+
+		glue.command(["set", "field", "length", eveObj.sequence_length]);
+		
+	});
+
+
 });
 
