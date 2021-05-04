c1beaa391d484586af15dbb472381cfaecd6bd3a
Rob Gifford <robjgiff@gmail.com>
2021-05-04 19:14:24 +0100

    Refactor build

--- glue/eve/loadLocusData2.js
+++ glue/eve/loadLocusData2.js
@@ -1,3 +1,11 @@
+var refconDataPath = "tabular/eve/efv-refseq-side-data.tsv";
+var source_name = 'fasta-digs-eve';
+
+// Load the refcon data and store relationships between locus and viral taxonomy
+var efvRefseqResultMap  = {};
+get_refcon_data(efvRefseqResultMap, refconDataPath);
+glue.log("INFO", "RESULT WAS ", efvRefseqResultMap);
+ 
 // Load flavivirus EVE data from tab file 
 var loadResult;
 glue.inMode("module/flaviviridaeTabularUtility", function() {
@@ -7,11 +15,12 @@ glue.inMode("module/flaviviridaeTabularUtility", function() {
 
 _.each(loadResult, function(eveObj) {
 
-	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
+    var sequenceID = eveObj.sequenceID;
+	glue.inMode("custom-table-row/locus_data/"+sequenceID, function() {
 
 	    glue.log("INFO", "eveObj result was:", eveObj);
 
-
+  
 		glue.command(["set", "field", "locus_id", eveObj.locus_numeric_id]);	
 		glue.command(["set", "field", "duplicate_id", eveObj.duplicate_id]);	
 		glue.command(["set", "field", "organism", eveObj.organism]);	
@@ -22,16 +31,58 @@ _.each(loadResult, function(eveObj) {
 
 
 	});
-	
 
-	glue.inMode("sequence/fasta-digs-eve/"+eveObj.sequenceID, function() {
+
+	if (eveObj.locus_name != 'NK') { // Skip elements that haven't been assigned to a locus
 	
-		glue.command(["set", "field", "name", eveObj.sequenceID]);
-		glue.command(["set", "field", "full_name", eveObj.sequenceID]);
-		glue.command(["set", "field", "length", eveObj.sequence_length]);
+		// Does an alignment exist for this locus ID
+		glue.log("INFO", "Getting taxonomic data for sequence:", sequenceID);
+
+		// Get the taxonomy 
+		var locus_numeric_id = eveObj.locus_numeric_id;
 		
-	});
+		
+		var efvRefConObj = efvRefseqResultMap[sequenceID];
+		glue.log("INFO", "LOADED REFCON INFO ", efvRefConObj);
+	
+		var subfamily   = efvRefConObj.subfamily;
+		var genus       = efvRefConObj.genus;
+		var clade       = efvRefConObj.clade;
 
+		glue.inMode("sequence/"+source_name+"/"+sequenceID, function() {
+
+			glue.command(["set", "field", "name", sequenceID]);
+			glue.command(["set", "field", "full_name", sequenceID]);
+			glue.command(["set", "field", "length", eveObj.sequence_length]);
+			glue.command(["set", "field", "subfamily", subfamily]);
+			glue.command(["set", "field", "genus", genus]);
+			glue.command(["set", "field", "clade", clade]);
+
+		});
+			
+	}
 
 });
 
+
+
+
+// get a list of sequence IDs from a given source in an alignment
+function get_refcon_data(resultMap, refconDataPath) {
+
+  // Load EVE reference data from tab file 
+  var loadResult;
+  glue.inMode("module/flaviviridaeTabularUtility", function() {
+	  loadResult = glue.tableToObjects(glue.command(["load-tabular", refconDataPath]));
+	  //glue.log("INFO", "load result was:", loadResult);
+  });
+
+  _.each(loadResult, function(eveObj) {
+
+	  var sequenceID = eveObj.sequenceID;
+	  //glue.log("INFO", "Setting locus data for EVE reference:", eveObj.sequenceID);
+	  resultMap[sequenceID] = eveObj;
+	
+  });
+  
+}
