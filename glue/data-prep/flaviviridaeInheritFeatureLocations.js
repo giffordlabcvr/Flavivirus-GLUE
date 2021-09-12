eeffc5f225c3d4201f9523bd4947f97f429b4578
Rob Gifford <robjgiff@gmail.com>
2021-09-12 10:05:38 +0100

    Refactor

---
+++ glue/data-prep/flaviviridaeInheritFeatureLocations.js
@@ -0,0 +1,28 @@
+//list of parent features which should be transferred - the parent of the list elements is 'whole_genome'
+var codingFeaturesToInherit = ["structural_proteins", "flavi-capsid",
+                               "premembrane", "flavi-envelope", "non_structural_proteins", 
+                               "NS1", "flavi-NS2A", "flavi-NS2B",
+                                "NS3", "flavi-NS4A", "flavi-NS4B", "2K", "NS5"];
+
+//list all HIV-1 reference sequences
+var refSeqObjs = glue.tableToObjects(glue.command(["list", "reference", "name"]));
+
+_.each(refSeqObjs, function(refSeqObj) {
+
+	if (refSeqObj.name == 'REF_EPEV' || refSeqObj.name == 'REF_SOKV' || refSeqObj.name == 'REF_SOKV') {
+
+		for(var k = 0; k < codingFeaturesToInherit.length; k++) {
+			var featureID = codingFeaturesToInherit[k];
+			glue.logInfo(" Inheriting feature: "+featureID+" from REF_YFV to "+refSeqObj.name);		
+
+			glue.inMode("reference/"+refSeqObj.name, function() {
+				glue.command(["inherit", "feature-location", 			
+					"AL_Flaviviridae_UNCONSTRAINED", "-l", "REF_YFV", featureID]);
+			});
+			
+		}
+	}
+
+});
+
+
